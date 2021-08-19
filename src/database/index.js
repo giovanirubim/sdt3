ecd92f8df656c46c341c8cd1f5530e251273394a
renanbertolazo <renan_bertolazo@hotmail.com>
2021-08-19 18:07:53 -0300

    Banco/FuncoesBanco

--- src/database/index.js
+++ src/database/index.js
@@ -8,16 +8,38 @@ class Database {
 	async addUser ({ name, email, password }) {
 		const { conn } = this;
 		// TODO
+		let sql = "INSERT INTO Usuario(name, email, password) VALUES ?";
+
+        const values = [name, email, password];
+        sql = mysql.format(sql, values);
+
+        const result = await promisify(conn.query).bind(conn)(sql);
+        console.log(result);
+        return result.length > 0 ? result[0]:null;
 	}
 	
 	async getUserByEmail ({ email }) {
 		const { conn } = this;
 		// TODO
+		let sql = "SELECT FROM Usuario WHERE Usuario.email=?";
+
+        const values = [email];
+        sql = mysql.format(sql, values);
+
+        const result = await promisify(conn.query).bind(conn)(sql);
+        console.log(result);
+        return result.length > 0 ? result[0]:null;
 	}
 	
 	async setTotpSecret ({ userId, secret }) {
 		const { conn } = this;
 		// TODO
+		let sql = " UPDATE Usuario SET secret=?, WHERE Usuario.id=?";
+        let values = [secret, userId];
+
+		const result = await promisify(conn.query).bind(conn)(sql);
+        console.log(result);
+        return result.length > 0 ? result[0]:null;
 	}
 }
 
