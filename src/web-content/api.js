b516bd76da428a0a2ed40ca917001b57d26d621c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-04 16:46:06 -0300

    API Connector

---
+++ src/web-content/api.js
@@ -0,0 +1,71 @@
+class BadRequest extends Error {
+	constructor() {
+		super('Bad request');
+	}
+};
+
+class Unauthorized extends Error {
+	constructor() {
+		super('Unauthorized');
+	}
+};
+
+class NotFound extends Error {
+	constructor() {
+		super('Not found');
+	}
+};
+
+class API {
+	static async request(method, path, data) {
+		const config = {
+			url: path,
+			method,
+		};
+		if (data != null) {
+			config.contentType = 'application/json; charset=utf-8';
+			config.data = JSON.stringify(data);
+		}
+		try {
+			return await $.ajax(config);
+		} catch(error) {
+			switch (error.status) {
+				case 400: throw new BadRequest();
+				case 401: throw new Unauthorized();
+				case 404: throw new NotFound();
+			}
+			throw error;
+		}
+	}
+
+	// Methods
+	static get(path, data) {
+		return this.request('GET', path, data);
+	}
+	static post(path, data) {
+		return this.request('POST', path, data);
+	}
+	static delete(path, data) {
+		return this.request('DELETE', path, data);
+	}
+
+	// Endpoints
+	static register(data) {
+		return this.post('/api/user', data);
+	}
+	static login(data) {
+		return this.post('/api/login', data);
+	}
+	static checkLogin() {
+		return this.get('/api/login');
+	}
+	static logOut() {
+		return this.delete('/api/login');
+	}
+	static createOtpStep() {
+		return this.post('/api/login/otp');
+	}
+	static removeOtpStep() {
+		return this.delete('/api/login/otp');
+	}
+}
